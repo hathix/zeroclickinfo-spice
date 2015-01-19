e602b5db42ed2d24c6158fa60ea146e4bea21d29
neelmehta <neelmehta@college.harvard.edu>
2015-01-19 23:50:30 +0000

    Trying out Perl sentence capitalization

--- lib/DDG/Spice/Headquarters.pm
+++ lib/DDG/Spice/Headquarters.pm
@@ -8,17 +8,8 @@ spice to => 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&conti
 spice wrap_jsonp_callback => 1;
 
 handle remainder => sub {
-    if $_ {
-        # uppercase each word in string for better Wikipedia compatibility
-        @words = split '', $_;
-        @ucwords = ();
-        foreach $word (@words) {
-            push @ucwords, ucfirst $word;
-        }
-
-        $uc = join ' ', @ucwords;
-        
-        return $uc;
+    if($_){
+        return $_;
     }
     return;
 };
