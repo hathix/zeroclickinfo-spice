b254cc116a8716ef91f4c24bdde41f4d948feeec
neelmehta <neelmehta@college.harvard.edu>
2015-01-20 02:52:35 +0000

    Added metadata and extended docs

--- lib/DDG/Spice/Headquarters.pm
+++ lib/DDG/Spice/Headquarters.pm
@@ -1,8 +1,24 @@
 package DDG::Spice::Headquarters;
-# ABSTRACT: Returns the headquarters of the given company.
+# ABSTRACT: Shows the headquarters of the given company.
 
 use DDG::Spice;
 
+# Metadata
+name "Headquarters";
+description "Shows the headquarters of the given company.";
+source "Wikipedia";
+icon_url "/i/wikipedia.org.ico";
+
+primary_example_queries "headquarters of duckduckgo", "twitter headquarters";
+secondary_example_queries "facebook hq";
+category "geography";
+topics "everyday", "geography";
+
+attribution twitter => ["https://twitter.com/hathix", "Neel Mehta"],
+            github  => ["https://github.com/hathix", "Neel Mehta"],
+            web     => ["http://hathix.com", "Neel Mehta"];
+
+# Real code
 triggers startend => 'headquarters of', 'headquarters', 'hq', 'hq of', 'corporate headquarters of', 'corporate headquarters', ;
 spice to => 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&continue=&rvprop=content&format=json&redirects=1&titles=$1';
 spice wrap_jsonp_callback => 1;
@@ -16,9 +32,24 @@ sub trim($)
 	return $string;
 }
 
+# uppercases the first letter in each word in the string; e.g. "duck duck go" => "Duck Duck Go"
+sub sentencecase($)
+{
+    my @words = split /[ -]/, shift;
+    my @ucwords = ();
+    
+    my $word;
+    foreach $word (@words){
+        push @ucwords, ucfirst $word;
+    }
+    
+    return join ' ', @ucwords;
+}
+
 handle remainder => sub {
     if($_){
-        return trim $_;
+        # sentence case plays better with wikipedia's api
+        return sentencecase trim $_;
     }
     return;
 };
--- share/spice/headquarters/headquarters.js
+++ share/spice/headquarters/headquarters.js
@@ -1,81 +1,83 @@
-(function (env) {
+(function(env) {
     "use strict";
-    env.ddg_spice_headquarters = function(api_result){
-        console.log("api", api_result);
-        
-        // grab content
-        var query = api_result.query;
-        if(!query) return Spice.failed('headquarters');
-        var pages = query.pages;
-        if(!pages) return Spice.failed('headquarters');
+    env.ddg_spice_headquarters = function(api_result) {
 
-        // grab first page
+        // drill down the api_result to get at the raw wikitext content
+        // it's at api_result.query.pages.{FIRST CHILD}.revisions[0]["*"]
+        var query = api_result.query;
+        if (!query) return Spice.failed('headquarters');
+        var pages = query.pages;
+        if (!pages) return Spice.failed('headquarters');
+
+        // the page has some random id, but it's the only child of pages
         var page;
-        for(var pageId in pages){
-          page = pages[pageId];
-          break;
+        for (var pageId in pages) {
+            page = pages[pageId];
+            break;
         }
-        
         if (!page) {
             return Spice.failed('headquarters');
         }
-        
+
+        // while we're at it, grab the company's name, which is the wikipedia page title
         var companyName = page.title;
-        if(!companyName) return Spice.failed('headquarters');
+        if (!companyName) return Spice.failed('headquarters');
         var revisions = page.revisions;
-        if(!revisions) return Spice.failed('headquarters');
+        if (!revisions) return Spice.failed('headquarters');
         var revision = revisions[0];
-        if(!revision) return Spice.failed('headquarters');
+        if (!revision) return Spice.failed('headquarters');
         var content = revision["*"];
-        if(!content) return Spice.failed('headquarters');
- 
+        if (!content) return Spice.failed('headquarters');
+
         // find each set of {{ ... }}
-        var stack = []; 
+        // use a stack to store the indices of the open {{'s
+        // each }} will remove the most recent {{ (the last item added to the queue)
+        // the actual text of the sets is stored in the sets array
+        var stack = [];
         var sets = [];
         var length = content.length;
 
-        for (var i = 0; i < length; /* nothing */){
-          if (content.charAt(i) + content.charAt(i+1) === "{{"){
-            // beginning of a set
-            stack.push(i);
-            i += 2;
-          }
-          else if(content.charAt(i) + content.charAt(i-1) === "}}"){
-            // found the ending of a set; start of it was stack.pop()
-            var start = stack.pop();
-            var end = i;
+        for (var i = 0; i < length; /* no update since we do it manually */ ) {
+            if (content.charAt(i) + content.charAt(i + 1) === "{{") {
+                // beginning of a set
+                stack.push(i);
+                i += 2;
+            } else if (content.charAt(i) + content.charAt(i - 1) === "}}") {
+                // found the ending of a set; start of it was stack.pop()
+                var start = stack.pop();
+                var end = i;
 
-            var set = content.substring(start, end+1);
-            sets.push(set);
+                var set = content.substring(start, end + 1);
+                sets.push(set);
 
-            i += 2;
-          }
-          else {
-            // neither start nor finish
-            i++;
-          }
+                i += 2;
+            } else {
+                // neither start nor end
+                i++;
+            }
         }
 
-        // find the first infobox, which is the set that begins with the harbinger
+        // find the first infobox, which is a set that begins with {{Infobox
         var harbinger = "{{Infobox";
         var infoboxText = null;
-        for (var i = 0; i < sets.length; i++){
+        for (var i = 0; i < sets.length; i++) {
             var set = sets[i];
-            if (set.indexOf(harbinger) === 0){
+            if (set.indexOf(harbinger) === 0) {
                 infoboxText = set;
                 break;
             }
         }
 
-        // must have infobox
-        if (!infoboxText){
+        // answer must have infobox
+        if (!infoboxText) {
             return Spice.failed('headquarters');
         }
 
         // split infobox into items (name, location, date founded, etc.)
+        // these are {key: value} pairs stored in the infobox object
         var items = infoboxText.split("\n");
         var infobox = {}; // each item is { title: text }
-        for (var i = 0; i < items.length; i++){
+        for (var i = 0; i < items.length; i++) {
             var item = items[i];
             var divide = item.indexOf("=");
             if (divide > -1) {
@@ -89,34 +91,35 @@
         }
 
         /* ** BEGIN HEADQUARTERS-SPECIFIC CODE ** */
-        
-        console.log("info", infobox);
 
         // Cleans up a Wikipedia-formatted string, which can contain [[ ... ]] and <ref>'s.
-        var cleanse = function(str){
+        var cleanse = function(str) {
             // get rid of <ref>'s
             str = str.replace(/<ref[\S\s]+<\/ref>/g, "");
-            str = str.replace(/<ref[\S\s]+\/>/g, "");            
-            
+            str = str.replace(/<ref[\S\s]+\/>/g, "");
+
             // get rid of {{cite}}'s
-            str = str.replace(/{{[^}]+}}/g, "");            
+            str = str.replace(/{{[^}]+}}/g, "");
 
             // get rid of any {{nowrap|...}}} stuff
-            str = str.replace(/{{ *nowrap *\| *([^}]+)}}/g, function(match, group){
+            str = str.replace(/{{ *nowrap *\| *([^}]+)}}/g, function(match, group) {
                 return group;
             });
-            
-            // get rid of br's
-            str = str.replace(/,? *<br *\/?>/g, ", ");
 
-            // replace stuff in [[ ... ]] with original
-            str = str.replace(/\[\[([^\]]+)]]/g, function(match, group){
+            // get rid of <br>'s and replace with comma
+            str = str.replace(/,? *<br *\/?>/g, ", ");
+            // BUT if the line begins with parens, get rid of the comma.
+            // This tends to happen because we strip <br>'s, which messes up stuff like
+            // "New York<br>(US Headquarters)"
+            str = str.replace(/, \(/, "(");
+
+            // replace stuff in [[ ... ]] with original (i.e. de-linkify)
+            str = str.replace(/\[\[([^\]]+)]]/g, function(match, group) {
                 // if group has a |, return what's on the right side (that's the "display" version
                 var barIndex = group.indexOf("|");
-                if (barIndex > -1){
+                if (barIndex > -1) {
                     return group.substring(barIndex + 1).trim();
-                }
-                else {
+                } else {
                     return group;
                 }
             });
@@ -126,33 +129,29 @@
 
         var location = null;
         // walk down preference gradient until location found
-        if (infobox.hq_location_city && infobox.hq_location_country){
+        // wikipedia has several different ways to show headquarters location
+        // hq_location is newer than location, and city + country is more specific than just location
+        if (infobox.hq_location_city && infobox.hq_location_country) {
             location = cleanse(infobox.hq_location_city) + ", " + cleanse(infobox.hq_location_country);
-        }
-        else if (infobox.hq_location_city){
+        } else if (infobox.hq_location_city) {
             location = cleanse(infobox.hq_location_city);
-        }
-        else if(infobox.hq_location){
+        } else if (infobox.hq_location) {
             location = cleanse(infobox.hq_location);
-        }
-        else if (infobox.location_city && infobox.location_country){
+        } else if (infobox.location_city && infobox.location_country) {
             location = cleanse(infobox.location_city) + ", " + cleanse(infobox.location_country);
-        }
-        else if (infobox.location_city){
+        } else if (infobox.location_city) {
             location = cleanse(infobox.location_city);
-        }
-        else if(infobox.location){
+        } else if (infobox.location) {
             location = cleanse(infobox.location);
-        }
-        else {
+        } else {
             // nothing found
             return Spice.failed('headquarters');
         }
 
-        // answer = location
-        if(location){
+        // is the location
+        if (location) {
             // Success!
-            
+
             var data = {
                 name: companyName,
                 headquarters: location,
@@ -164,19 +163,18 @@
                 name: "Headquarters",
                 data: data,
                 meta: {
-                    // sourceName: "Wikipedia",
-                    // sourceUrl: 'https://en.wikipedia.org/wiki/' + companyName
+                    sourceName: "Google Maps",
+                    sourceUrl: 'https://www.google.com/maps?q=' + location
                 },
                 templates: {
                     group: 'base',
-                    options:{
+                    options: {
                         content: Spice.headquarters.content,
-                        moreAt: false
+                        moreAt: true
                     }
                 }
             });
-        }
-        else {
+        } else {
             return Spice.failed('headquarters');
         }
     };
